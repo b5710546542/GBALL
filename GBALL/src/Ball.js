9e92767d92e395bd310c800b6dbad794923f4207
Noot <jc.nut92@gmail.com>
2015-03-11 18:36:52 +0700

    have ground and one ball

---
+++ GBALL/src/Ball.js
@@ -0,0 +1,6 @@
+var Ball = cc.Sprite.extend({
+	ctor: function(){
+		this._super();
+		this.initWithFile('res/images/ball.png');
+	}
+});
---
+++ GBALL/src/GameLayer.js
@@ -0,0 +1,21 @@
+var GameLayer = cc.LayerColor.extend({
+    init: function() {
+        this._super( new cc.Color( 127, 127, 127, 255 ) );
+        this.setPosition( new cc.Point( 0, 0 ) );
+ 		
+        var ball = new Ball();
+        ball.setPosition( new cc.Point(200,200) );
+        this.addChild( ball );
+
+        return true;
+    }
+});
+ 
+var StartScene = cc.Scene.extend({
+    onEnter: function() {
+        this._super();
+        var layer = new GameLayer();
+        layer.init();
+        this.addChild( layer );
+    }
+});
